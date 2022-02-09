8caa6fb89a9081f6afd793f0306dcbfca6b9c2e6
Mm1OOO <95989781+Mm1OOO@users.noreply.github.com>
2022-02-09 02:48:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function shop() {
+    location.href = "../src/pages/shop.html"
+}
+function faq() {
+    location.href = "../src/pages/faq.html"
+} 
+function safety() {
+    location.href = "../src/pages/safety.html"
+}
+function contact() {
+    location.href = "../src/pages/contact.html"
+}
+function back() {
+    location.href = "../src/pages/index.html"
+}
+
+
+
